59fce6f375e0aa974b14701d7488e6bdd59f21a3
nayoung <nayoung8142@gmail.com>
2025-05-08 20:00:48 +0900

    feat: MySQL datasource 설정

--- src/main/java/com/gameet/GameetApplication.java
+++ src/main/java/com/gameet/GameetApplication.java
@@ -1,5 +1,6 @@
 package com.gameet;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GameetApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(GameetApplication.class, args);
     }
 
