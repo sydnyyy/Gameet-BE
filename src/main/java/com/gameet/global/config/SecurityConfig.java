24f07cc10c7bc084c051fb2ea77a6f7f6407c0a9
nayoung <nayoung8142@gmail.com>
2025-05-08 20:24:52 +0900

    feat: SecurityFilterChain 생성

---
+++ src/main/java/com/gameet/global/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.gameet.global.config;
+
+import com.gameet.auth.config.JwtAuthenticationFiler;
+import com.gameet.auth.jwt.JwtAuthenticationProvider;
+import com.gameet.auth.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(HttpMethod.POST, "/auth/sign-up/**", "/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFiler(), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    private JwtAuthenticationFiler jwtAuthenticationFiler() {
+        return new JwtAuthenticationFiler(jwtUtil, jwtAuthenticationProvider);
+    }
+}
