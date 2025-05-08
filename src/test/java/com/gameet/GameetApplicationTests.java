2df59ed36fd32283552b0e03e8fdb8b0fea11e11
nayoung <nayoung8142@gmail.com>
2025-05-08 20:01:15 +0900

    test: @SpringBootTest 제거

--- src/test/java/com/gameet/GameetApplicationTests.java
+++ src/test/java/com/gameet/GameetApplicationTests.java
@@ -1,9 +1,7 @@
 package com.gameet;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GameetApplicationTests {
 
     @Test
