875a7a06b77597345bca8c18fd9495c4fd2370d2
nayoung <nayoung8142@gmail.com>
2025-05-08 20:07:45 +0900

    feat: MethodArgumentNotValidException Handler 생성

--- src/main/java/com/gameet/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gameet/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.gameet.global.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,5 +16,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(ex.getErrorCode().getStatus())
                 .body(ex.getErrorCode().getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        FieldError fieldError = bindingResult.getFieldError();
+        String message = fieldError.getDefaultMessage();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
     }
 }
