f313d8fb466911a032511aa90d9ddd38004a476b
nayoung <nayoung8142@gmail.com>
2025-05-08 19:55:17 +0900

    Initial commit

---
+++ src/main/java/com/gameet/GameetApplication.java
@@ -0,0 +1,13 @@
+package com.gameet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameetApplication.class, args);
+    }
+
+}
