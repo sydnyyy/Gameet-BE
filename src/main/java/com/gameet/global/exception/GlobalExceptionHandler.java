876e6d4aeeea414ce4bc9537cf5f41093474f76e
nayoung <nayoung8142@gmail.com>
2025-05-14 16:17:37 +0900

    feat: ConstraintViolationException, HttpMessageNotReadableException Handler 설정

--- src/main/java/com/gameet/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gameet/global/exception/GlobalExceptionHandler.java
@@ -1,12 +1,17 @@
 package com.gameet.global.exception;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -24,5 +29,25 @@ public class GlobalExceptionHandler {
         FieldError fieldError = bindingResult.getFieldError();
         String message = fieldError.getDefaultMessage();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handleConstraintViolationException(ConstraintViolationException ex) {
+        String message = ex.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .findFirst()
+                .orElse("잘못된 요청입니다.");
+
+        return ResponseEntity.badRequest().body(message);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleMessageNotReadable(HttpMessageNotReadableException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(Map.of(
+                        "error", "Bad Request",
+                        "message", ex.getMessage()));
     }
 }
