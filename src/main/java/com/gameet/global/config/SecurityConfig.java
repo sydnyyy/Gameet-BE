d1bc70c45a1352db8568e470e0786576bd68b6e5
song <jefkdjsf2@naver.com>
2025-05-17 01:46:47 +0900

    [FEAT] 웹소켓 End Point permitAll 설정

--- src/main/java/com/gameet/global/config/SecurityConfig.java
+++ src/main/java/com/gameet/global/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .logout(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/ws/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/sign-up/**", "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users/profile").hasRole("GUEST")
                         .requestMatchers(HttpMethod.PUT, "/users/profile").hasRole("USER")
