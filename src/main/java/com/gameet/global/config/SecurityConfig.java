ac867cf96c93a8b863398bb27c66813b515cb533
nayoung <nayoung8142@gmail.com>
2025-05-23 04:32:47 +0900

    feat: /error 경로 접근 허용

--- src/main/java/com/gameet/global/config/SecurityConfig.java
+++ src/main/java/com/gameet/global/config/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig {
             "/api/users/auth/login",
             "/api/users/auth/password-reset/**",
             "/api/users/profile/nickname-available",
-            "/api/users/auth/token/refresh"
+            "/api/users/auth/token/refresh",
+            "/error"
     };
 }
