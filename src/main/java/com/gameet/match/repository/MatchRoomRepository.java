48afb0976ab325f76a40d77b7f2921a8a88924bd
song <jefkdjsf2@naver.com>
2025-05-25 00:00:20 +0900

    [FEAT] 매칭 REPOSITORY 추가

---
+++ src/main/java/com/gameet/match/repository/MatchRoomRepository.java
@@ -0,0 +1,8 @@
+package com.gameet.match.repository;
+
+import com.gameet.match.entity.MatchRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatchRoomRepository extends JpaRepository<MatchRoom, Long> {
+
+}
