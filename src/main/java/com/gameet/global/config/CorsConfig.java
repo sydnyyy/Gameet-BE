c2867fab3dec25dc60822659e23f95f7fd70c32a
song <jefkdjsf2@naver.com>
2025-05-31 01:19:45 +0900

    [FIX] ExposedHeaders "Password-Reset-Token" 설정

--- src/main/java/com/gameet/global/config/CorsConfig.java
+++ src/main/java/com/gameet/global/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
-        configuration.setExposedHeaders(List.of("Authorization"));
+        configuration.setExposedHeaders(List.of("Authorization", "Password-Reset-Token"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
