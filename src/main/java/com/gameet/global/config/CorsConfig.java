d1da3a8759042b74b0fef7d2b2ff7adbc4e008c1
song <jefkdjsf2@naver.com>
2025-06-20 00:58:34 +0900

    [CHORE] cors 테스트

--- src/main/java/com/gameet/global/config/CorsConfig.java
+++ src/main/java/com/gameet/global/config/CorsConfig.java
@@ -14,10 +14,10 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8000"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+//        configuration.setAllowCredentials(true);
         configuration.setExposedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
