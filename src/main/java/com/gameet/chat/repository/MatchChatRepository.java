87266df8713d0066c9eac7488eb4a94c1cbea4fd
hyemi.kim <hemi33379@gmail.com>
2025-06-19 17:50:56 +0900

    [FIX] 입장메시지 제외

--- src/main/java/com/gameet/chat/repository/MatchChatRepository.java
+++ src/main/java/com/gameet/chat/repository/MatchChatRepository.java
@@ -17,16 +17,18 @@ public interface MatchChatRepository extends JpaRepository<MatchChat, Long> {
     List<MatchChat> findByMatchRoomId(@Param("matchRoomId") Long matchRoomId);
 
     @Query("""
-    SELECT COUNT(c)
-    FROM MatchChat c
-    WHERE c.matchParticipant.matchRoom.matchRoomId = :matchRoomId
-      AND c.matchParticipant.matchParticipantId <> :participantId
-      AND c.sendAt > :lastReadAt
-""")
+        SELECT COUNT(c)
+        FROM MatchChat c
+        WHERE c.matchParticipant.matchRoom.matchRoomId = :matchRoomId
+          AND c.matchParticipant.matchParticipantId <> :participantId
+          AND c.messageType <> 'ENTER'
+          AND c.sendAt > :lastReadAt
+    """)
     long countUnreadMessages(
               @Param("matchRoomId") Long matchRoomId,
               @Param("participantId") Long myMatchParticipantId,
               @Param("lastReadAt") LocalDateTime lastReadAt
     );
+
 
 }
