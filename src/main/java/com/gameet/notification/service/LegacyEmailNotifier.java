28f583a2b029e9ea51261f7ade452eca7643ae4f
sydnyyy <nayoung8142@naver.com>
2025-08-03 02:33:29 +0900

    chore: EmailNotifier -> LegacyEmailNotifier 클래스명 변경

--- src/main/java/com/gameet/notification/service/LegacyEmailNotifier.java
+++ src/main/java/com/gameet/notification/service/LegacyEmailNotifier.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class EmailNotifier {
+public class LegacyEmailNotifier {
 
     private final JavaMailSender javaMailSender;
 
